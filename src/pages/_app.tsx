5b877727a5826b55cdc0bfb8c65329b73354cd0c
Raphael Marques <raphaeljcmarques@gmail.com>
2023-04-30 10:24:48 -0300

    feat: header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,24 @@
-import { globalStyles } from '@/styles/global'
-import type { AppProps } from 'next/app'
-
-import { Roboto } from 'next/font/google'
+import logoImg from '@/assets/logo.svg';
+import { globalStyles } from '@/styles/global';
+import * as S from '@/styles/pages/app';
+import type { AppProps } from 'next/app';
+import { Roboto } from 'next/font/google';
 
 const roboto = Roboto({
   subsets: ['latin'],
-  weight: ['400', '700']
-})
+  weight: ['400', '700'],
+});
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={roboto.className}>
+    <S.Container className={roboto.className}>
+      <S.Header>
+        <img src={logoImg.src} alt="" />
+      </S.Header>
+
       <Component {...pageProps} />
-    </div>
-  )
+    </S.Container>
+  );
 }
