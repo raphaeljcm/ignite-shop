82bf8e6f4e8b503abaaadff0f143304cc700e13b
Raphael Marques <raphaeljcmarques@gmail.com>
2023-04-30 15:06:27 -0300

    feat: product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,23 @@
+import * as S from '@/styles/pages/product';
+
 export default function Product() {
-  return <h1>product</h1>;
+  return (
+    <S.ProductContainer>
+      <S.ImageContainer></S.ImageContainer>
+
+      <S.ProductDetails>
+        <h1>Camisa X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+          consectetur blanditiis doloremque sit molestiae suscipit nemo
+          nesciunt! Adipisci illo, tempore, repellendus amet ipsum officia aut
+          quas, dignissimos nostrum nam eaque!
+        </p>
+
+        <button type="button">Comprar agora</button>
+      </S.ProductDetails>
+    </S.ProductContainer>
+  );
 }
